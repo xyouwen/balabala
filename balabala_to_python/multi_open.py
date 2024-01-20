38ed922ae88b3d37ee7b74b695f99b3d5477b5b2
xyouwen <xyouwen@foxmail.com>
2024-01-20 09:55:21 +0800

    update readme

---
+++ balabala_to_python/multi_open.py
@@ -0,0 +1,8 @@
+import wftools
+path = "C:\Program Files\Tencent\WeChat\WeChat.exe"
+# path = r"D:\Program Files\Netease\MuMuPlayer-12.0\shell\MuMuPlayer.exe"
+# path = r"C:\Program Files (x86)\DingDing\DingtalkLauncher.exe"
+wftools.open_soft(soft_path=path, num=2)
+
+
+# 经过实测，该库不能打开模拟器
